b7ba5525bd9b3ade7c83a837d4644259ac45d53d
atknin <atknini@yahoo.com>
2016-11-05 10:13:59 +0300

    работает бот

--- mysite/HRONION/urls.py
+++ mysite/HRONION/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.conf.urls import url
-import views
+import HRONION.views as views
 
 urlpatterns = [ 
 	url(r'^start/', views.start),
--- mysite/HRONION/views.py
+++ mysite/HRONION/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 import os, sys
 from django.contrib import auth
 from django.http import Http404, HttpResponse 
-import bot_inform # информация в месенджере телеграмм: bot_inform.sent_to_atknin_bot(massage, telegram_whom)
+import HRONION.bot_inform as bot_inform #  информация в месенджере телеграмм: bot_inform.sent_to_atknin_bot(massage, telegram_whom)
 telegram_whom = "v"#v - ваня d - дима, остальное любое вдоем
 # Create your views here.
 def start(request):
